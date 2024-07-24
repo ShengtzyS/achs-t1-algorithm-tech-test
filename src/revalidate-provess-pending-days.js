b929e3daa3059a7d207da0a7bf4c21b077986659
ShengtzyS <165830738+ShengtzyS@users.noreply.github.com>
2024-07-24 16:22:20 -0400

    prueba técnica entrevista

--- src/revalidate-provess-pending-days.js
+++ src/revalidate-provess-pending-days.js
@@ -1,3 +1,24 @@
-export function getRevalidateProcessPendingDays() {
-	return [];
+
+export function getRevalidateProcessPendingDays(today, mainProcessLastExecutionDayOfMonth, revalidateProcessLastExecutionDayOfMonth) {
+  const WARMUP_DAYS = 2; 
+  let lastNotRevalidatedDayOfMonth = today - WARMUP_DAYS;
+  let startPendingForRevalidateDayOfMonth = revalidateProcessLastExecutionDayOfMonth + 1;
+  let lastPendingForRevalidateDayOfMonth = Math.min(mainProcessLastExecutionDayOfMonth, lastNotRevalidatedDayOfMonth);
+
+  if (startPendingForRevalidateDayOfMonth > 30 || startPendingForRevalidateDayOfMonth < 1 || lastPendingForRevalidateDayOfMonth < 1) {
+      return [];
+  }
+
+  let pendingDays = [];
+  if (revalidateProcessLastExecutionDayOfMonth === mainProcessLastExecutionDayOfMonth && today - WARMUP_DAYS > mainProcessLastExecutionDayOfMonth) {
+      for (let i = startPendingForRevalidateDayOfMonth; i <= today; i++) {
+          pendingDays.push(i);
+      }
+  } else {
+      for (let i = startPendingForRevalidateDayOfMonth; i <= lastPendingForRevalidateDayOfMonth; i++) {
+          pendingDays.push(i);
+      }
+  }
+
+  return pendingDays;
 }
--- src/revalidate-provess-pending-days.spec.js
+++ src/revalidate-provess-pending-days.spec.js
@@ -2,15 +2,49 @@ import { describe, expect, test } from 'vitest';
 import { getRevalidateProcessPendingDays } from './revalidate-provess-pending-days.js';
 
 describe(getRevalidateProcessPendingDays, () => {
-	test('example', () => {
+	test('revalidateProcessLastExecutionDayOfMonth < mainProcessLastExecutionDayOfMonth && today - warm-up days === mainProcessLastExecutionDayOfMonth', () => {
+		
 		// prepare
-		const input = {};
-		const expected = [];
-
+		const today = 17; 
+		const mainProcessLastExecutionDayOfMonth = 15;
+		const revalidateProcessLastExecutionDayOfMonth = 10;
+		const expected = [11, 12, 13, 14, 15]; 
+	
 		// act
-		const result = getRevalidateProcessPendingDays(input);
-
+		const result = getRevalidateProcessPendingDays(today, mainProcessLastExecutionDayOfMonth, revalidateProcessLastExecutionDayOfMonth);
+	
 		// assert
 		expect(result).toStrictEqual(expected);
+	});
+	
+
+    test('revalidateProcessLastExecutionDayOfMonth < mainProcessLastExecutionDayOfMonth && today - warm-up days > mainProcessLastExecutionDayOfMonth', () => {
+        
+		// prepare
+        const today = 18;
+        const mainProcessLastExecutionDayOfMonth = 15;
+        const revalidateProcessLastExecutionDayOfMonth = 10;
+        const expected = [11, 12, 13, 14, 15]; 
+
+        // act
+        const result = getRevalidateProcessPendingDays(today, mainProcessLastExecutionDayOfMonth, revalidateProcessLastExecutionDayOfMonth);
+
+        // assert
+        expect(result).toStrictEqual(expected);
+    });
+
+	test('revalidateProcessLastExecutionDayOfMonth === mainProcessLastExecutionDayOfMonth && today - warm-up days > mainProcessLastExecutionDayOfMonth', () => {
+		
+		// prepare
+		const today = 18; 
+		const mainProcessLastExecutionDayOfMonth = 15;
+		const revalidateProcessLastExecutionDayOfMonth = 15;
+		const expected = [16, 17, 18]; 
+	
+		// act
+		const result = getRevalidateProcessPendingDays(today, mainProcessLastExecutionDayOfMonth, revalidateProcessLastExecutionDayOfMonth);
+	
+		// assert
+		expect(result).toEqual(expected);
 	});
 });
